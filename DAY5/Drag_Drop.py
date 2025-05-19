5008c7a7453f400796cd3d90065144d7eaa7fa93
ANILCHAUDHARY <anil.bhumal@gmail.com>
2025-05-19 22:11:29 +0545

    DAY 5 commit

--- DAY5/Drag_Drop.py
+++ DAY5/Drag_Drop.py
@@ -41,23 +41,25 @@ Prevent_Propogation.click()
 time.sleep(2)
 Drag_Me_btn1= driver.find_element(By.XPATH,"//div[text()='Drag Me']")
 notGreedyOuter_droppable1= driver.find_element(By.XPATH,"(//p[text()='Outer droppable'])[1]")
-action.drag_and_drop(Drag_Me_btn1,notGreedyOuter_droppable1).perform()
+action.drag_and_drop(Drag_Me_btn1,notGreedyOuter_droppable1).release().perform()
 
 time.sleep(3)
 notGreedy_innerDroppable=driver.find_element(By.ID,"notGreedyInnerDropBox")
-action.drag_and_drop(Drag_Me_btn1,notGreedy_innerDroppable).perform()
+action.drag_and_drop(Drag_Me_btn1,notGreedy_innerDroppable).release().perform()
 
 time.sleep(2)
 Drag_Me_btn2= driver.find_element(By.XPATH,"//div[text()='Drag Me']")
 greedyOuter_droppable2= driver.find_element(By.XPATH,"//div[@id='greedyDropBox']")
-action.drag_and_drop(Drag_Me_btn2,greedyOuter_droppable2).perform()
+action.drag_and_drop(Drag_Me_btn2,greedyOuter_droppable2).release().perform()
 
 time.sleep(3)
 Greedy_innerDroppable=driver.find_element(By.XPATH,"//div[@id='greedyDropBoxInner']")
-action.drag_and_drop(Drag_Me_btn2,Greedy_innerDroppable).perform()
+driver.execute_script("arguments[0].scrollIntoView(true);",Greedy_innerDroppable)
+action.drag_and_drop(Drag_Me_btn2,Greedy_innerDroppable).release().perform()
 
 time.sleep(3)
 Revert_draggable= driver.find_element(By.ID,"droppableExample-tab-revertable")
+driver.execute_script("arguments[0].scrollIntoView(true);",Revert_draggable)
 driver.execute_script("arguments[0].click()",Revert_draggable)
 
 time.sleep(3)
@@ -65,6 +67,9 @@ Will_Revert= driver.find_element(By.ID,"revertable")
 WillNotRevert= driver.find_element(By.ID,"notRevertable")
 DropBox= driver.find_element(By.XPATH,"(//div[@id='droppable'])[3]")
 
+
 action.drag_and_drop(Will_Revert,DropBox).perform()
 time.sleep(3)
 action.drag_and_drop(WillNotRevert,DropBox).perform()
+
+driver.quit()
